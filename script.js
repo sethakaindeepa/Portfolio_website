fe948ae8bc3cd78b12d2cfbe4e7a9520574c8f4d
sethakaindeepa <sethakaindeepa@gmail.com>
2026-04-19 22:00:24 +0530

    Connect contact form to Gmail

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        mobile: document.getElementById("mobile").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send("service_wjwedfp", "template_oeitdlb", params)
+        .then(function (response){
+          alert("Email sent successfully!");
+        }, function (error){
+          alert("Failed to send email!");
+        });
+
+}
