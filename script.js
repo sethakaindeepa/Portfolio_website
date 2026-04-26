6c37e8fc1370d9caaeab5e2091a17301e5293840
sethakaindeepa <sethakaindeepa@gmail.com>
2026-04-26 09:53:18 +0530

    Add navbar icon to mobile

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ function sendMail() {
         });
 
 }
+
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", () =>{
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll("#nav-menu a").forEach(link =>{
+    link.addEventListener("click", () =>{
+        navMenu.classList.remove("active");
+    });
+});
